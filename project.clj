17a3d4f4f7f085c1b3a88239380d21dcd30f4fa3
ntalbs <ntalbsen@gmail.com>
2013-12-16 09:39:43 +0900

    change license to MIT as in LICENSE file.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject tweetbot "0.1.0"
   :description "Tweet Bot"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [twitter-api "0.7.4"]
                  [overtone/at-at "1.2.0"]
