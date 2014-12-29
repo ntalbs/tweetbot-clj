df1747282bb15cea315d2dc0a15a272ddd9522c7
ntalbs <ntalbsen@gmail.com>
2014-12-29 09:04:28 +0900

    oauth 정보를 환경변수에서 읽도록 수정

--- src/tweetbot/config.clj
+++ src/tweetbot/config.clj
@@ -1,9 +1,9 @@
 (ns tweetbot.conf)
 
 (def conf
-  {:oauth-settings {:consumer-key "consumer-key"
-                    :consumer-secret "consumer-secret"
-                    :access-token "access-token"
-                    :access-token-secret "access-token-secret"}
+  {:oauth-settings {:consumer-key (System/getenv "TWEETBOT_TWITTER_CONSUMER_KEY"),
+                    :consumer-secret (System/getenv "TWEETBOT_TWITTER_CONSUMER_SECRET"),
+                    :access-token (System/getenv "TWEETBOT_TWITTER_ACCESS_TOKEN"),
+                    :access-token-secret (System/getenv "TWEETBOT_TWITTER_ACCESS_TOKEN_SECRET")}
    :db-uri "db-url"
    :tweet-interval (* 1000 60 60)})
